c6dbf30acf1018735a54eee79761a21900a8878c
raheena-raeen <42466721+raheena-raeen@users.noreply.github.com>
2020-04-06 12:35:58 +0545

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace App2
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            System.Threading.Timer tim = new System.Threading.Timer(obj=>OnCallBack(), null, 0, 1000);
+        }
+        private void OnCallBack()
+        {
+            TextView tv = FindViewById<TextView>(Resource.Id.timenow);
+            tv.Text = System.DateTime.UtcNow.ToString("f");
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
